8043ed3297ea5adfa1969d89d895c7346234fb75
SHADOW <davronbekn202@gmail.com>
2025-12-15 23:48:34 +0500

    js update

---
+++ jas/learn.js
@@ -0,0 +1,32 @@
+// var age = 10;
+// let name = "Davronbek";
+// const lastname = "salom";
+// age = 18;
+// name = "shadow";
+// const io = 0;
+// for(;;){
+//     console.log(io)
+// }
+
+let word = "HelloWorld";
+let result = word.lastIndexOf('o')
+let result2 = word.indexOf('o')
+let result3 = word.slice(1,4)
+let result4 = word.substr(2,6)
+let result5 = word.replace("e","a")
+let result6 = word.charAt()
+let result7 = word.toUpperCase()
+let result8 = word.toLowerCase()
+let result9 = word.trim()
+let result10 = word.split("")
+
+
+console.log(result10)
+
+
+
+
+
+
+
+// console.log(lastname,name,age)
--- main/link.js
+++ main/link.js
@@ -1,18 +1,34 @@
-let secret = Math.floor(Math.random() * 10) + 1;
-let guess;
-let attempts = 0;
+// let secret = Math.floor(Math.random() * 10) + 1;
+// let guess;
+// let attempts = 0;
 
-while (guess !== secret) {
-    guess = Number(prompt("Guess a number between 1 and 10"));
-    attempts++;
+// while (guess !== secret) {
+//     guess = Number(prompt("Guess a number between 1 and 10"));
+//     attempts++;
 
-    if (guess > secret) {
-        alert("Too high!");
-    } else if (guess < secret) {
-        alert("Too low!");
-    } else {
-        alert(`Correct! You guessed it in ${attempts} tries.`);
-    }
+//     if (guess > secret) {
+//         alert("Too high!");
+//     } else if (guess < secret) {
+//         alert("Too low!");
+//     } else {
+//         alert(`Correct! You guessed it in ${attempts} tries.`);
+//     }
+// }
+
+while (true){
+    let a = Number(prompt("First number:"));
+    if (a === 0) break;
+    let op = prompt("Operator (+ - * /):");
+    let b = Number(prompt("Second number:"));
+
+    let result;
+
+    if (op === "+") result = a + b;
+    if (op === "-") result = a - b;
+    if (op === "*") result = a * b;
+    if (op === "/") result = a / b;
+
+    alert("Result: " + result);
+
 }
-
 
